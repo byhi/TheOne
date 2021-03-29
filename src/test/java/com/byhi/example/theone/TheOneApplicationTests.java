8082d6b799d0034900849194624dfd0da12bd8b6
byhi <byhisoft@gmail.com>
2021-03-29 14:16:13 +0200

    first commit

---
+++ src/test/java/com/byhi/example/theone/TheOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byhi.example.theone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
